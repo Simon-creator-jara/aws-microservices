2ce15ff1aa1c2b3cf8a36f3c4caa0cb7efb292f0
Simon-creator-jara <70153636+Simon-creator-jara@users.noreply.github.com>
2024-04-08 19:52:58 -0500

    add lmbda event trigger

--- lib/aws-microservices-stack.ts
+++ lib/aws-microservices-stack.ts
@@ -1,16 +1,68 @@
 import * as cdk from 'aws-cdk-lib';
+import { LambdaRestApi } from 'aws-cdk-lib/aws-apigateway';
+import { AttributeType, BillingMode, Table } from 'aws-cdk-lib/aws-dynamodb';
+import { Runtime } from 'aws-cdk-lib/aws-lambda';
+import { NodejsFunction, NodejsFunctionProps } from 'aws-cdk-lib/aws-lambda-nodejs';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { join } from 'path';
 
 export class AwsMicroservicesStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const productTable = new Table(this, 'product',{
+      partitionKey: {
+        name: 'id',
+        type: AttributeType.STRING
+      },
+      tableName: 'product',
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      billingMode: BillingMode.PAY_PER_REQUEST
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'AwsMicroservicesQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const NodejsFunctionProps: NodejsFunctionProps={
+      bundling:{
+        externalModules :[
+          'aws-sdk'
+        ]
+      },
+      environment: {
+        PRIMARY_KEY: 'id',
+        DYNAMO_TABLE_NAME: productTable.tableName
+      },
+      runtime: Runtime.NODEJS_16_X
+    }
+
+    const productFunction = new NodejsFunction(this, 'productLambdaFunction',{
+      entry: join(__dirname,`/../src/product/index.js`),
+      ...NodejsFunctionProps,
+    })
+
+    productTable.grantReadWriteData(productFunction);
+
+    //product
+    //GET /product
+    //POST /product
+
+    //GET /product/{id}
+    //PUT /product/{id}
+    //DELETE /product/{id}
+
+    const apigw = new LambdaRestApi(this, 'productApi', {
+      restApiName: 'Product Service',
+      handler: productFunction,
+      proxy: false
+    });
+
+    const product = apigw.root.addResource('product');
+    product.addMethod('GET');
+    product.addMethod('POST');
+
+    const singleProduct = product.addResource('{id}');
+    singleProduct.addMethod('GET');
+    singleProduct.addMethod('PUT');
+    singleProduct.addMethod('DELETE');
+
+
   }
 }
